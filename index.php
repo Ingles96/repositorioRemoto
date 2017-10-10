eff3e91028a9947fd5a557f2f0add2f2f28fecdb
jogich <2daw@WINMAC11.alumnado.flo>
2017-10-10 10:58:00 +0200

    add script

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css" href="ximo.css">
+
     <title></title>
   </head>
   <body>
 
   </body>
-
+  <script type="text/javascript" src="ximo.js"></script>
 </html>
