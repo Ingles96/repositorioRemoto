137171c7a6c6aa14bfb46f2dcdd21a5264e58637
jogich <2daw@WINMAC11.alumnado.flo>
2017-10-10 11:04:58 +0200

    cambio titulo

--- index.php
+++ index.php
@@ -4,8 +4,11 @@
     <meta charset="utf-8">
 
     <title></title>
+    <a>psps.com</a>
   </head>
   <body>
+
+  <h1> Titulo de Ximo </h1>
 
   </body>
   <script type="text/javascript" src="ximo.js"></script>
